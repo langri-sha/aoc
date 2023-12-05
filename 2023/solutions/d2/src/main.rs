80edd8c0fcdf4f44e9428b80e5af8c2b8d28df53
Filip Dupanović <filip.dupanovic@gmail.com>
2023-12-05 08:29:22 +0000

    Solve part two

--- 2023/solutions/d2/src/main.rs
+++ 2023/solutions/d2/src/main.rs
@@ -6,15 +6,17 @@ type Contents = (usize, usize, usize);
 
 fn main() {
     println!("{}", part_one((12, 13, 14)));
+    println!("{}", part_two());
 }
 
 fn part_one(limit: Contents) -> usize {
-    let result =
-        read_cubes().enumerate().filter_map(|(game, seen)| {
+    let result = read_cubes()
+        .enumerate()
+        .filter_map(|(game, seen)| {
             if seen.0 <= limit.0 && seen.1 <= limit.1 && seen.2 <= limit.2 {
-                return Some(game + 1)
+                return Some(game + 1);
             } else {
-                return None
+                return None;
             }
         })
         .sum::<usize>();
@@ -22,33 +24,40 @@ fn part_one(limit: Contents) -> usize {
     return result;
 }
 
+fn part_two() -> usize {
+    let result = read_cubes()
+        .enumerate()
+        .map(|(_, seen)| [seen.0, seen.1, seen.2].iter().product::<usize>())
+        .sum::<usize>();
+
+    return result;
+}
+
 fn read_cubes() -> impl Iterator<Item = Contents> {
-    read_lines()
-        .map(|line| {
-            let mut seen = (0, 0, 0);
+    read_lines().map(|line| {
+        let mut seen = (0, 0, 0);
 
-            line.unwrap()
-                .split(": ")
-                .nth(1)
-                .unwrap()
-                .split(|c| c == ',' || c == ';')
-                .for_each(|item| {
-                    let [value, color, ..] = match item.trim().split(" ").collect::<Vec<&str>>()[..]
-                    {
-                        [value, color, ..] => [value, color],
-                        _ => panic!("Invalid item: {}", item),
-                    };
+        line.unwrap()
+            .split(": ")
+            .nth(1)
+            .unwrap()
+            .split(|c| c == ',' || c == ';')
+            .for_each(|item| {
+                let [value, color, ..] = match item.trim().split(" ").collect::<Vec<&str>>()[..] {
+                    [value, color, ..] => [value, color],
+                    _ => panic!("Invalid item: {}", item),
+                };
 
-                    match color {
-                        "red" => seen.0 = seen.0.max(value.parse::<usize>().unwrap()),
-                        "green" => seen.1 = seen.1.max(value.parse::<usize>().unwrap()),
-                        "blue" => seen.2 = seen.2.max(value.parse::<usize>().unwrap()),
-                        _ => panic!("Invalid color: {}", color),
-                    };
-                });
+                match color {
+                    "red" => seen.0 = seen.0.max(value.parse::<usize>().unwrap()),
+                    "green" => seen.1 = seen.1.max(value.parse::<usize>().unwrap()),
+                    "blue" => seen.2 = seen.2.max(value.parse::<usize>().unwrap()),
+                    _ => panic!("Invalid color: {}", color),
+                };
+            });
 
-            return seen
-        })
+        return seen;
+    })
 }
 
 fn read_lines() -> io::Lines<io::BufReader<File>> {
