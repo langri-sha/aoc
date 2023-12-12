750cbecaddefd06833cc54683727a27738d59927
Filip Dupanović <filip.dupanovic@gmail.com>
2023-12-12 01:22:30 +0000

    Solve part two

--- 2023/solutions/d3/src/main.rs
+++ 2023/solutions/d3/src/main.rs
@@ -7,6 +7,7 @@ use std::usize;
 
 fn main() {
     println!("Part one: {}", part_one());
+    println!("Part two: {}", part_two());
 }
 
 fn part_one() -> usize {
@@ -34,6 +35,45 @@ fn part_one() -> usize {
         .sum::<usize>()
 }
 
+fn part_two() -> usize {
+    let re_symbol = Regex::new(r"\*").unwrap();
+
+    let grid = read_lines().map(|i| i.unwrap()).collect::<Vec<String>>();
+
+    grid.iter()
+        .enumerate()
+        .fold(Vec::new(), |mut hits, (row, line)| {
+            for cap in re_symbol.captures_iter(line) {
+                let col = cap.get(0).unwrap().start();
+
+                hits.push(
+                    neighbours(row, col, &grid)
+                        .filter_map(|(x, y)| {
+                            if grid[x].chars().nth(y).unwrap().is_ascii_digit() {
+                                return Some(numbers(&grid, x, vec![y]).first().unwrap().to_owned());
+                            }
+
+                            return None;
+                        })
+                        .collect::<HashSet<usize>>(),
+                );
+            }
+
+            hits
+        })
+        .into_iter()
+        .filter_map(|hits| {
+            if hits.len() == 2 {
+                return Some(
+                    hits.iter().product::<usize>(),
+                );
+            } else {
+                return None;
+            }
+        })
+        .sum::<usize>()
+}
+
 fn neighbours(row: usize, col: usize, grid: &Vec<String>) -> impl Iterator<Item = (usize, usize)> {
     [
         (row - 1, col - 1),
@@ -57,15 +97,18 @@ fn neighbours(row: usize, col: usize, grid: &Vec<String>) -> impl Iterator<Item 
     .into_iter()
 }
 
-fn numbers(grid: &Vec<String>, row: usize, col: HashSet<usize>) -> Vec<usize> {
+fn numbers<T>(grid: &Vec<String>, row: usize, col: T) -> Vec<usize>
+where
+    T: IntoIterator<Item = usize> + Clone,
+{
     let re_nums = Regex::new(r"\d+").unwrap();
 
     re_nums
         .captures_iter(grid[row].as_str())
         .map(|c| c.get(0).unwrap())
         .fold(Vec::new(), |mut caps, c| {
-            for num in col.iter() {
-                if num >= &&c.start() && num <= &&c.end() {
+            for num in col.clone().into_iter() {
+                if num >= c.start() && num <= c.end() {
                     caps.push(c.as_str());
                     break;
                 }
