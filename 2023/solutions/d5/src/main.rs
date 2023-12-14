fe0a4fb0844e86fbf543afe608efdbeaed30ee58
Filip Dupanović <filip.dupanovic@gmail.com>
2023-12-14 13:19:32 +0000

    Format code

--- 2023/solutions/d5/src/main.rs
+++ 2023/solutions/d5/src/main.rs
@@ -10,58 +10,66 @@ fn main() {
 fn part_one() -> usize {
     let (seeds, mappings) = read_seeds_mappings();
 
-
     find_lowest_location(seeds, mappings)
 }
 
 fn read_seeds_mappings() -> (Vec<usize>, Vec<Vec<Vec<usize>>>) {
     read_lines().fold((Vec::new(), Vec::new()), |(seeds, mut mappings), line| {
-            let line = line.unwrap();
+        let line = line.unwrap();
 
-            if line.is_empty() {
-                return (seeds, mappings);
-            }
+        if line.is_empty() {
+            return (seeds, mappings);
+        }
 
-            if line.starts_with("seeds") {
-                return (
-                    line.split_ascii_whitespace()
-                        .skip(1)
-                        .map(|s| s.parse::<usize>().unwrap())
-                        .collect::<Vec<_>>(),
-                    mappings,
-                );
-            }
+        if line.starts_with("seeds") {
+            return (
+                line.split_ascii_whitespace()
+                    .skip(1)
+                    .map(|s| s.parse::<usize>().unwrap())
+                    .collect::<Vec<_>>(),
+                mappings,
+            );
+        }
 
-            if line.chars().next().unwrap().is_ascii_alphabetic() {
-                mappings.push(Vec::new());
-            } else {
-                let last = mappings.last_mut().unwrap();
+        if line.chars().next().unwrap().is_ascii_alphabetic() {
+            mappings.push(Vec::new());
+        } else {
+            let last = mappings.last_mut().unwrap();
 
-                last.push(
-                    line.split_ascii_whitespace()
-                        .map(|s| s.parse::<usize>().unwrap())
-                        .collect::<Vec<_>>(),
-                );
-            }
+            last.push(
+                line.split_ascii_whitespace()
+                    .map(|s| s.parse::<usize>().unwrap())
+                    .collect::<Vec<_>>(),
+            );
+        }
 
-            (seeds, mappings)
-        })
+        (seeds, mappings)
+    })
 }
 
 fn find_lowest_location(seeds: Vec<usize>, mappings: Vec<Vec<Vec<usize>>>) -> usize {
-    seeds.into_iter().map(|seed| mappings.iter().fold(seed, |seed, mapping| {
-        mapping.iter().find_map(|mappings| {
-            let [destination, source, length] = mappings[..] else {
-                panic!("Invalid mapping: {:?}", mappings);
-            };
+    seeds
+        .into_iter()
+        .map(|seed| {
+            mappings.iter().fold(seed, |seed, mapping| {
+                mapping
+                    .iter()
+                    .find_map(|mappings| {
+                        let [destination, source, length] = mappings[..] else {
+                            panic!("Invalid mapping: {:?}", mappings);
+                        };
 
-            if (seed >= source) && (seed <= (source + length)) {
-                Some(destination + (seed - source))
-            } else {
-                None
-            }
-        }).unwrap_or(seed)
-    })).min().unwrap()
+                        if (seed >= source) && (seed <= (source + length)) {
+                            Some(destination + (seed - source))
+                        } else {
+                            None
+                        }
+                    })
+                    .unwrap_or(seed)
+            })
+        })
+        .min()
+        .unwrap()
 }
 
 fn read_lines() -> io::Lines<io::BufReader<File>> {
