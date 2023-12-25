f083be59a4281f4d44cbc5761192918b7701bce3
Filip Dupanović <filip.dupanovic@gmail.com>
2023-12-25 23:26:10 +0000

    Solve part two

--- 2023/solutions/d9/src/main.rs
+++ 2023/solutions/d9/src/main.rs
@@ -4,31 +4,49 @@ use std::path::Path;
 
 fn main() {
     println!("{}", part_one());
+    println!("{}", part_two());
 }
 
 fn part_one() -> isize {
-    read_lines()
-        .map(|line| {
-            line.unwrap()
-                .split(" ")
-                .map(|s| s.parse::<isize>().unwrap())
-                .collect::<Vec<_>>()
-        })
-        .fold(0isize, |acc, history| {
-            let mut tails: Vec<_> = Vec::new();
-            let mut next: Vec<_> = history.clone();
+    get_histories().fold(0isize, |acc, history| {
+        let mut tails: Vec<_> = Vec::new();
+        let mut next: Vec<_> = history.clone();
 
-            while !next.iter().all(|&v| v == 0) {
-                tails.push(next.last().unwrap().to_owned());
+        while !next.iter().all(|&v| v == 0) {
+            tails.push(next.last().unwrap().to_owned());
 
-                next = next
-                    .windows(2)
-                    .map(|w| w[1] - w[0])
-                    .collect::<Vec<_>>();
-            }
+            next = next.windows(2).map(|w| w[1] - w[0]).collect::<Vec<_>>();
+        }
 
-            acc + tails.iter().sum::<isize>()
-        })
+        acc + tails.iter().sum::<isize>()
+    })
+}
+
+fn part_two() -> isize {
+    get_histories().fold(0isize, |acc, history| {
+        let mut heads: Vec<_> = Vec::new();
+        let mut next: Vec<_> = history.clone();
+
+        while !next.iter().all(|&v| v == 0) {
+            heads.push(next.first().unwrap().to_owned());
+
+            next = next.windows(2).map(|w| w[1] - w[0]).collect::<Vec<_>>();
+        }
+
+        acc + heads
+            .iter()
+            .rev()
+            .fold(0isize, |acc, &v| v - acc)
+    })
+}
+
+fn get_histories() -> impl Iterator<Item = Vec<isize>> {
+    read_lines().map(|line| {
+        line.unwrap()
+            .split(" ")
+            .map(|s| s.parse::<isize>().unwrap())
+            .collect::<Vec<_>>()
+    })
 }
 
 fn read_lines() -> io::Lines<io::BufReader<File>> {
