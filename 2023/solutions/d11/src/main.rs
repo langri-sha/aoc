7da7dadf8a375e43e5d67dd3c4d189b570548b7f
Filip Dupanović <filip.dupanovic@gmail.com>
2024-01-05 10:07:48 +0000

    Solve part two

--- 2023/solutions/d11/src/main.rs
+++ 2023/solutions/d11/src/main.rs
@@ -4,21 +4,19 @@ use std::path::Path;
 
 fn main() {
     println!("{}", part_one());
+    println!("{}", part_two());
 }
 
 fn part_one() -> usize {
-    let mut lines: Vec<Vec<char>> = read_lines()
-        .map(|line| line.unwrap())
-        .map(|line| line.chars().collect::<Vec<_>>())
-        .fold(Vec::new(), |mut acc, line| {
-            if line.iter().all(|c| c == &'.') {
-                acc.push(line.clone());
-            }
+    let mut lines = read_map().fold(Vec::new(), |mut acc, line| {
+        if line.iter().all(|c| c == &'.') {
+            acc.push(line.clone());
+        }
 
-            acc.push(line);
+        acc.push(line);
 
-            acc
-        });
+        acc
+    });
 
     let expanded: Vec<usize> = (0..lines.first().unwrap().len())
         .filter(|n| lines.iter().all(|line| line[*n] == '.'))
@@ -55,6 +53,70 @@ fn part_one() -> usize {
                 .map(|(x2, y2)| x2.abs_diff(*x1) + y2.abs_diff(*y1))
                 .sum::<usize>()
         })
+}
+
+fn part_two() -> usize {
+    let lines: Vec<Vec<char>> = read_map().collect();
+
+    let expanded_lines: Vec<usize> = lines
+        .iter()
+        .enumerate()
+        .filter_map(|(i, line)| {
+            if line.iter().all(|c| c == &'.') {
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let expanded_columns: Vec<usize> = (0..lines.first().unwrap().len())
+        .filter(|n| lines.iter().all(|line| line[*n] == '.'))
+        .collect();
+
+    let mut galaxies = lines
+        .iter()
+        .enumerate()
+        .fold(Vec::new(), |mut acc, (y, line)| {
+            line.iter().enumerate().for_each(|(x, &c)| {
+                if c == '#' {
+                    acc.push((x, y));
+                }
+            });
+
+            acc
+        });
+
+    galaxies.sort_by(|a, b| a.0.cmp(&b.0).then_with(|| a.1.cmp(&b.1)));
+
+    galaxies
+        .iter()
+        .enumerate()
+        .fold(0usize, |acc, (i, (x1, y1))| {
+            acc + galaxies
+                .iter()
+                .skip(i + 1)
+                .map(|(x2, y2)| {
+                    let x1offset =
+                        expanded_columns.iter().filter(|&e| e.lt(&x1)).count() * (1_000_000 - 1);
+                    let x2offset =
+                        expanded_columns.iter().filter(|&e| e.lt(&x2)).count() * (1_000_000 - 1);
+                    let y1offset =
+                        expanded_lines.iter().filter(|&e| e.lt(&y1)).count() * (1_000_000 - 1);
+                    let y2offset =
+                        expanded_lines.iter().filter(|&e| e.lt(&y2)).count() * (1_000_000 - 1);
+
+                    (x2 + x2offset).abs_diff(*x1 + x1offset)
+                        + (y2 + y2offset).abs_diff(*y1 + y1offset)
+                })
+                .sum::<usize>()
+        })
+}
+
+fn read_map() -> impl Iterator<Item = Vec<char>> {
+    read_lines()
+        .map(|line| line.unwrap())
+        .map(|line| line.chars().collect::<Vec<_>>())
 }
 
 fn read_lines() -> io::Lines<io::BufReader<File>> {
